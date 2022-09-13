ca0d04af54968b88b8f25dc3bf1fb35aed44536a
21a95a0420 <109516055+21a95a0420@users.noreply.github.com>
2022-09-13 11:59:50 +0530

    n=int(input()) a=0 b=1 c=[] for i in range(0,n):     temp=a     c.append(a)     a=b     b=temp+a if n in c:     print('True') else:     print('False')

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,16 +1,13 @@
 n=int(input())
 a=0
 b=1
-i=1
 c=[]
-while i<=n:
+for i in range(0,n):
     temp=a
     c.append(a)
     a=b
     b=temp+a
-    i+=1
 if n in c:
     print('True')
 else:
     print('False')
-    
