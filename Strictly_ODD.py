045a8d3d3aa78509b59f4de02861d4972a268535
21a95a0420 <109516055+21a95a0420@users.noreply.github.com>
2022-09-19 22:18:44 +0530

    n=int(input()) kal=list(map(int,input().split())) c=0 d=0 for i in range (len(kal)):     if kal[i]%2==1:         c+=1         if i%2==1:             d+=1         else:             break if c==d:     print('True') else:     print('False')

---
+++ Strictly_ODD.py
@@ -0,0 +1,15 @@
+n=int(input())
+kal=list(map(int,input().split()))
+c=0
+d=0
+for i in range (len(kal)):
+    if kal[i]%2==1:
+        c+=1
+        if i%2==1:
+            d+=1
+        else:
+            break
+if c==d:
+    print('True')
+else:
+    print('False')
