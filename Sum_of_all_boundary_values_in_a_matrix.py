e3ff88605ebdbd00dfd988af994d0db5f1d04920
21a95a0420 <109516055+21a95a0420@users.noreply.github.com>
2022-10-09 15:16:43 +0530

    m,n=map(int,input().split()) x=[] for i in range(m):     x.append(list(map(int,input().split()))) c=0 d=0 for i in range(m):     for j in range(n):         if i!=0 and j!=0 and j!=n-1 and i!=m-1:             c+=x[i][j]     d+=sum(x[i]) print(d-c)

---
+++ Sum_of_all_boundary_values_in_a_matrix.py
@@ -0,0 +1,12 @@
+m,n=map(int,input().split())
+x=[]
+for i in range(m):
+    x.append(list(map(int,input().split())))
+c=0
+d=0
+for i in range(m):
+    for j in range(n):
+        if i!=0 and j!=0 and j!=n-1 and i!=m-1:
+            c+=x[i][j]
+    d+=sum(x[i])
+print(d-c)
